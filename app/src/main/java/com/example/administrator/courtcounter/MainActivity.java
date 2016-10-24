7ae36a3da22126ae2c8021a93aede99d5eefcff0
paulcarla <paul.carla@pusd.us>
2016-10-24 14:40:11 -0700

    Milestone2 complete

--- app/src/main/java/com/example/administrator/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/administrator/courtcounter/MainActivity.java
@@ -2,12 +2,24 @@ package com.example.administrator.courtcounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView textViewTeam1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textViewTeam1 = (TextView)findViewById( R.id.textViewAdd1 );
+        textViewTeam1.setText("0");
     }
+
+    public void clickAdd1(View view) {
+        textViewTeam1.setText("1");
+    }
+
+
 }
