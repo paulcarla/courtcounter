9e071f4628d40a9316c14f8cf05623ba9820976a
paulcarla <paul.carla@pusd.us>
2016-10-26 14:48:51 -0700

    milestone 6

--- app/src/main/java/com/example/administrator/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/administrator/courtcounter/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.administrator.courtcounter;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
     private TextView textViewTeam2;
     int a;
     int b;
+    AlertDialog gameOverAlert;
 
 
     @Override
@@ -24,6 +27,8 @@ public class MainActivity extends AppCompatActivity {
         a=0;
         textViewTeam2.setText(""+b);
 
+        gameOverAlert= new AlertDialog.Builder(MainActivity.this).create();
+
     }
 
     public void clickAdd1(View view) {
@@ -35,10 +40,24 @@ public class MainActivity extends AppCompatActivity {
         textViewTeam2.setText(""+b);
     }
 
-    public void clickReset(View view) {
+    public void clickGameOver(View view) {
+        gameOverAlert.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+        gameOverAlert.setTitle("Game Over!");
+        gameOverAlert.setMessage("The game is a tie!");
+        if (a > b) {
+            gameOverAlert.setMessage("Team 1 wins");
+        }if (b > a) {
+            gameOverAlert.setMessage("Team 2 wins");
+        }
         textViewTeam1.setText("0");
         textViewTeam2.setText("0");
         a=0;
         b=0;
+        gameOverAlert.show();
     }
 }
